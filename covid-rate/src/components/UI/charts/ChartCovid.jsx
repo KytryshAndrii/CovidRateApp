e8ef3c21e6393f5414d84cf388d54240356efc42
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-05-12 20:19:27 +0200

    Chart Done. Next step -> Customizasion

--- covid-rate/src/components/UI/charts/ChartCovid.jsx
+++ covid-rate/src/components/UI/charts/ChartCovid.jsx
@@ -1,62 +1,52 @@
 import React from "react";
-import React, { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
+import { Chart, CategoryScale, LineController, LineElement, PointElement, LinearScale} from "chart.js"
+import { Line } from "react-chartjs-2";
 
-const DynamicChart = ({coviddata}) => {
-  const [chartData, setChartData] = useState({});
+const DynamicChart = (coviddata) => {
 
-  const Chart = () => {
-    let xValues = [];
-    let confirmed = [];
-    let deaths = [];
+  if(!coviddata.coviddata){
+    return (<p></p>);
+  }
 
-        setChartData({
-          labels: xValues,
-          datasets: [
-            {
-              label: "confirmed COVID cases",
-              data: confirmed,
-              backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-              borderColor: ["rgba(255, 99, 132, 1)"],
-              borderWidth: 1
-            },
-            {
-              label: "deaths",
-              data: deaths,
-              backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-              borderColor: ["rgba(54, 162, 235, 0.2)"],
-              borderWidth: 1
-            }
-          ]
-        });
-      }
-  };
+  Chart.register(
+    CategoryScale, 
+    LineController,
+    LineElement, 
+    PointElement, 
+    LinearScale, 
+  )
 
-  useEffect(() => {
-    Chart();
-  }, []);
+  var labels = []
+  var data = []
+  console.log(coviddata.coviddata)
+  coviddata.coviddata.map((element) => {
+    labels.push(element.Date);
+    data.push(element.Cases);
+  });
+
+  console.log(data.splice((data.length)-100, data.length));
+  console.log(labels.slice((labels.length)-100, labels.length));
+
+ var  chartData = {
+    labels: labels.slice((labels.length)-100, labels.length),
+    datasets: [
+      {
+        label: "Covid Cases",
+        data: data.splice((data.length)-100, data.length),
+        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)"],
+        borderWidth: 1
+        },
+    ]};
 
   return (
     <div className="Chart">
       <h1>Bar Chart</h1>
       <div>
-        <Bar
+        <Line
           data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "THICCNESS SCALE", display: true },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }
-              ]
-            }
-          }}
         />
       </div>
     </div>
-  );
+  ); };
 export default DynamicChart;
