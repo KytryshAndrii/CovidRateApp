ec0e553d4a6371a4059f5dead340d04764321fa2
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-05-16 21:31:14 +0200

    Changed api request added new header, shanged css stat-block property

--- covid-rate/src/API/CasesService.jsx
+++ covid-rate/src/API/CasesService.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 export default class CasesService {
 
     static async getCountries(){
-        const response = await axios.get("https://api.covid19api.com/countries");
+        const response = await axios.get("https://api.covid19api.com/countries", {
+            headers: {"Access-Control-Allow-Origin": "*"}
+        });
         var arr = []
         for(let i = 0; i < response.data.length; i++){
             arr.push({label: response.data[i].Country, value: response.data[i].ISO2});
@@ -12,7 +14,9 @@ export default class CasesService {
     }
 
     static async getCovidRateByCountry(datefrom, dateto, status, country){
-        const response = await axios.get("https://api.covid19api.com/country/"+country+"/status/"+status+"?from="+datefrom+"T00:00:00Z"+"&to="+dateto+"T00:00:00Z");
+        const response = await axios.get("https://api.covid19api.com/country/"+country+"/status/"+status+"?from="+datefrom+"T00:00:00Z"+"&to="+dateto+"T00:00:00Z", {
+            headers: {"Access-Control-Allow-Origin": "*"}
+        });
         if(Object.keys(response.data).length < 1){
             return 0;
         }
@@ -20,7 +24,9 @@ export default class CasesService {
     }
     
     static async getWorldSummaryCovidRate(){
-        const response = await axios.get("https://api.covid19api.com/summary");
+        const response = await axios.get("https://api.covid19api.com/summary", {
+            headers: {"Access-Control-Allow-Origin": "*"}
+        });
         if(Object.keys(response.data.Global).length < 1){
             return 0;
         };
